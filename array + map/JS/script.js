b82baf35f7bf17151d9ea17b2d03f5857c5be608
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-10 21:20:10 -0300

    exercicio map + array

---
+++ array + map/JS/script.js
@@ -0,0 +1,32 @@
+// Array contendo 10 valores de salário
+const salarios = [1500, 2200, 1800, 2800, 1900, 2100, 2500, 1700, 3000, 2400];
+
+// Método map() para aplicar aumento de salários
+const salariosComAumento = salarios.map(salario => {
+  if (salario <= 2000) {
+    return salario * 1.15; // 15% de aumento
+  } else {
+    return salario * 1.1; // 10% de aumento
+  }
+});
+
+// Método filter() para obter salários acima de 2500
+const salariosAcima2500 = salariosComAumento.filter(salario => salario > 2500);
+
+// Método reduce() para somar os salários acima de 2500
+const totalSalariosAcima2500 = salariosAcima2500.reduce((total, salario) => total + salario, 0);
+
+// Exibir resultados no console
+console.log("Salários com aumento:", salariosComAumento);
+console.log("Salários acima de 2500:", salariosAcima2500);
+console.log("Total dos salários acima de 2500:", totalSalariosAcima2500);
+
+// Explicanção código 
+// No meu arquivo em JS (script.js), foi criado um array salarios contendo os valores de salário iniciais.
+
+// E usando o método map() para percorrer o array salarios e aplicar os aumentos de salário 
+
+// Método filter() para criar um novo array salarios que deve ser Acima2500 que contém apenas os 
+// salários acima de 2500.
+
+// E por fim usei o método reduce() para somar os valores do array salariosAcima2500.
---
+++ array + map/JS/script2.js
@@ -0,0 +1,83 @@
+// Código feito para a interação simples, para se gerar uma lista de tarefas
+
+// Definindo a matriz: armazenar tarefas 
+const taskList = [];
+
+// Função para adicionar uma nova tarefa
+function addTask() { // Usada para adicionar novas tarefas 
+    console.log("Adicionando nova tarefa... "); 
+    const description = document.getElementById('description').value;
+    const author = document.getElementById('author').value;
+    const department = document.getElementById('department').value;
+    const importance = parseInt(document.getElementById('importance').value);
+
+    const task = {
+        description: description,
+        author: author,
+        department: department,
+        importance: importance,
+        value: null,
+        duration: null
+    };
+
+    taskList.push(task); //insere dados
+    renderTaskList();
+    clearForm(); // limpa os campos de formulário
+}
+
+// Função para renderizar a lista de tarefas
+function renderTaskList() {
+    const taskListBody = document.getElementById('taskList');
+    taskListBody.innerHTML = '';
+
+    taskList.map((task, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${task.description}</td>
+            <td>${task.author}</td>
+            <td>${task.department}</td>
+            <td>${task.importance}</td>
+            <td>${task.value ? task.value : ''}</td>
+            <td>${task.duration ? task.duration : ''}</td>
+            <td>
+                <button onclick="addValueToTask(${index})">Valor</button>
+                <button onclick="addDurationToTask(${index})">Duração</button>
+                <button onclick="removeTask(${index})">Remover</button>
+            </td>
+        `;
+
+        taskListBody.appendChild(row);
+    });
+}
+
+// Funções addValueToTask, addDurationToTask, removeTask, clearForm
+function addValueToTask(index) {
+    const value = prompt('Informe o valor da tarefa:');
+    if (value !== null) {
+        taskList[index].value = value;
+        renderTaskList();
+    }
+}
+
+function addDurationToTask(index) {
+    const duration = prompt('Informe a duração da tarefa:');
+    if (duration !== null) {
+        taskList[index].duration = duration;
+        renderTaskList();
+    }
+}
+
+function removeTask(index) {
+    taskList.splice(index, 1);
+    renderTaskList();
+}
+
+function clearForm() {
+    document.getElementById('description').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('department').value = '';
+    document.getElementById('importance').value = '';
+}
+
+// Atualização de lista de tarefas
+renderTaskList();
